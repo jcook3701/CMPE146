c2f91a217fb49e011f309a0d1d60e80a020f231b
jcook3701 <jcook3701@gmail.com>
2018-03-01 20:50:17 -0800

    fixed Ports

--- pwmDriver.cpp
+++ pwmDriver.cpp
@@ -6,25 +6,24 @@ static const uint8_t pwm_pin = 6;
 void PWMDriver::pwmSelectAllPins()
 {
   LPC_SC->PCONP |= (0x1 << pwm_pin); 
-
   //PWM_PIN_2_0
-  LPC_PINCON->PINSEL3 &= ~(0x3 << 4);
-  LPC_PINCON->PINSEL3 |= (0x2 << 4); 
+  LPC_PINCON->PINSEL4 &= ~(0x3 << 0);
+  LPC_PINCON->PINSEL4 |=  (0x2 << 0); 
   //PWM_PIN_2_1
-  LPC_PINCON->PINSEL3 &= ~(0x3 << 8);
-  LPC_PINCON->PINSEL3 |= (0x2 << 8); 
+  LPC_PINCON->PINSEL4 &= ~(0x3 << 2);
+  LPC_PINCON->PINSEL4 |=  (0x2 << 2); 
   //PWM_PIN_2_2
-  LPC_PINCON->PINSEL3 &= ~(0x3 << 10);
-  LPC_PINCON->PINSEL3 |= (0x2 << 10);
+  LPC_PINCON->PINSEL4 &= ~(0x3 << 4);
+  LPC_PINCON->PINSEL4 |=  (0x2 << 4);
   //PWM_PIN_2_3
-  LPC_PINCON->PINSEL3 &= ~(0x3 << 14);
-  LPC_PINCON->PINSEL3 |= (0x2 << 14);
+  LPC_PINCON->PINSEL4 &= ~(0x3 << 6);
+  LPC_PINCON->PINSEL4 |=  (0x2 << 6);
   //PWM_PIN_2_4
-  LPC_PINCON->PINSEL3 &= ~(0x3 << 16);
-  LPC_PINCON->PINSEL3 |= (0x2 << 16); 
+  LPC_PINCON->PINSEL4 &= ~(0x3 << 8);
+  LPC_PINCON->PINSEL4 |=  (0x2 << 8); 
   //PWM_PIN_2_5
-  LPC_PINCON->PINSEL3 &= ~(0x3 << 20);
-  LPC_PINCON->PINSEL3 |= (0x2 << 20);
+  LPC_PINCON->PINSEL4 &= ~(0x3 << 10);
+  LPC_PINCON->PINSEL4 |=  (0x2 << 10);
 }
 
 void PWMDriver::pwmSelectPin(PWMDriver::PWM_PIN pwm_pin_arg)
@@ -33,28 +32,28 @@ void PWMDriver::pwmSelectPin(PWMDriver::PWM_PIN pwm_pin_arg)
   switch(pwm_pin_arg)
     {
     case PWM_PIN_2_0:
-      LPC_PINCON->PINSEL3 &= ~(0x3 << 4);
-      LPC_PINCON->PINSEL3 |= (0x2 << 4); 
+      LPC_PINCON->PINSEL4 &= ~(0x3 << 0);
+      LPC_PINCON->PINSEL4 |=  (0x2 << 0); 
       break;
     case PWM_PIN_2_1:
-      LPC_PINCON->PINSEL3 &= ~(0x3 << 8);
-      LPC_PINCON->PINSEL3 |= (0x2 << 8); 
+      LPC_PINCON->PINSEL4 &= ~(0x3 << 2);
+      LPC_PINCON->PINSEL4 |=  (0x2 << 2); 
       break;
     case PWM_PIN_2_2:
-      LPC_PINCON->PINSEL3 &= ~(0x3 << 10);
-      LPC_PINCON->PINSEL3 |= (0x2 << 10); 
+      LPC_PINCON->PINSEL4 &= ~(0x3 << 4);
+      LPC_PINCON->PINSEL4 |=  (0x2 << 4); 
       break;
     case PWM_PIN_2_3:
-      LPC_PINCON->PINSEL3 &= ~(0x3 << 14);
-      LPC_PINCON->PINSEL3 |= (0x2 << 14); 
+      LPC_PINCON->PINSEL4 &= ~(0x3 << 6);
+      LPC_PINCON->PINSEL4 |=  (0x2 << 6); 
       break;
     case PWM_PIN_2_4:
-      LPC_PINCON->PINSEL3 &= ~(0x3 << 16);
-      LPC_PINCON->PINSEL3 |= (0x2 << 16); 
+      LPC_PINCON->PINSEL4 &= ~(0x3 << 8);
+      LPC_PINCON->PINSEL4 |=  (0x2 << 8); 
       break;
     case PWM_PIN_2_5:
-      LPC_PINCON->PINSEL3 &= ~(0x3 << 20);
-      LPC_PINCON->PINSEL3 |= (0x2 << 20); 
+      LPC_PINCON->PINSEL4 &= ~(0x3 << 10);
+      LPC_PINCON->PINSEL4 |=  (0x2 << 10); 
       break; 
     }
 }
