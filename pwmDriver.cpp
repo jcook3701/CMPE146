e41492c2159ad4db785cbfc96c23af19d9beb200
Peter Stapleton <peter@luno.hsd1.ca.comcast.net>
2018-03-01 20:31:09 -0800

    Fixed small errors

--- pwmDriver.cpp
+++ pwmDriver.cpp
@@ -3,31 +3,32 @@
 //bit 6 in LPCSRC->PCONP turns on/off the pwm
 static const uint8_t pwm_pin = 6; 
 
-PWMDriver::pwmSelectAllPins(){
+void PWMDriver::pwmSelectAllPins()
+{
   LPC_SC->PCONP |= (0x1 << pwm_pin); 
 
   //PWM_PIN_2_0
   LPC_PINCON->PINSEL3 &= ~(0x3 << 4);
   LPC_PINCON->PINSEL3 |= (0x2 << 4); 
   //PWM_PIN_2_1
-  LPC_PINCON->PINSEL3 & ~(0x3 << 8);
+  LPC_PINCON->PINSEL3 &= ~(0x3 << 8);
   LPC_PINCON->PINSEL3 |= (0x2 << 8); 
   //PWM_PIN_2_2
-  LPC_PINCON->PINSEL3 & ~(0x3 << 10);
+  LPC_PINCON->PINSEL3 &= ~(0x3 << 10);
   LPC_PINCON->PINSEL3 |= (0x2 << 10);
   //PWM_PIN_2_3
-  LPC_PINCON->PINSEL3 & ~(0x3 << 14);
+  LPC_PINCON->PINSEL3 &= ~(0x3 << 14);
   LPC_PINCON->PINSEL3 |= (0x2 << 14);
   //PWM_PIN_2_4
-  LPC_PINCON->PINSEL3 & ~(0x3 << 16);
+  LPC_PINCON->PINSEL3 &= ~(0x3 << 16);
   LPC_PINCON->PINSEL3 |= (0x2 << 16); 
   //PWM_PIN_2_5
-  LPC_PINCON->PINSEL3 & ~(0x3 << 20);
+  LPC_PINCON->PINSEL3 &= ~(0x3 << 20);
   LPC_PINCON->PINSEL3 |= (0x2 << 20);
 }
 
-PWMDriver::pwmSelectPin(PWMDriver::PWM_PIN pwm_pin_arg){
-
+void PWMDriver::pwmSelectPin(PWMDriver::PWM_PIN pwm_pin_arg)
+{
   LPC_SC->PCONP |= (0x1 << pwm_pin);   
   switch(pwm_pin_arg)
     {
@@ -36,36 +37,39 @@ PWMDriver::pwmSelectPin(PWMDriver::PWM_PIN pwm_pin_arg){
       LPC_PINCON->PINSEL3 |= (0x2 << 4); 
       break;
     case PWM_PIN_2_1:
-      LPC_PINCON->PINSEL3 & ~(0x3 << 8);
+      LPC_PINCON->PINSEL3 &= ~(0x3 << 8);
       LPC_PINCON->PINSEL3 |= (0x2 << 8); 
       break;
     case PWM_PIN_2_2:
-      LPC_PINCON->PINSEL3 & ~(0x3 << 10);
+      LPC_PINCON->PINSEL3 &= ~(0x3 << 10);
       LPC_PINCON->PINSEL3 |= (0x2 << 10); 
       break;
     case PWM_PIN_2_3:
-      LPC_PINCON->PINSEL3 & ~(0x3 << 14);
+      LPC_PINCON->PINSEL3 &= ~(0x3 << 14);
       LPC_PINCON->PINSEL3 |= (0x2 << 14); 
       break;
     case PWM_PIN_2_4:
-      LPC_PINCON->PINSEL3 & ~(0x3 << 16);
+      LPC_PINCON->PINSEL3 &= ~(0x3 << 16);
       LPC_PINCON->PINSEL3 |= (0x2 << 16); 
       break;
     case PWM_PIN_2_5:
-      LPC_PINCON->PINSEL3 & ~(0x3 << 20);
+      LPC_PINCON->PINSEL3 &= ~(0x3 << 20);
       LPC_PINCON->PINSEL3 |= (0x2 << 20); 
       break; 
     }
 }
 
-PWMDriver::pwmInitSingleEdgeMode(uint32_t frequency_Hz){
+void PWMDriver::pwmInitSingleEdgeMode(uint32_t frequency_Hz)
+{
   
 }
 
-PWMDriver::setDutyCycle(PWMDriver::PWM_PIN pwm_pin_arg, float duty_cycle_percentage){
+void PWMDriver::setDutyCycle(PWMDriver::PWM_PIN pwm_pin_arg, float duty_cycle_percentage)
+{
   
 }
 
-PWMDriver::setFrequency(uint32_t frequency_Hz){
+void PWMDriver::setFrequency(uint32_t frequency_Hz)
+{
   
 }
