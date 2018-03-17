4e3ae399f008c27ce2803bc97f63b4517c7f86ad
jcook3701 <jcook3701@gmail.com>
2018-03-16 22:13:45 -0700

    removed rules.sh from git and added to gitignore

--- rules.sh
+++
@@ -1,5 +0,0 @@
-#rules.sh environment variable template file
-#Do not include any ("quotation marks") in this file
-
-USER_DRIVER_DIR=/Users/jared3701/Documents/College/5th_year/CMPE_146/SJSU_Dev_mac/SJSU-Dev/firmware/lib/jared_lib
-USER_MAIN_DIR=/Users/jared3701/Documents/College/5th_year/CMPE_146/sjsu_dev_mac/SJSU-Dev/firmware/HelloWorld/L5_Application
