62c969aefd311bd7ca3941de774e605f2d962b88
jcook3701 <jcook3701@gmail.com>
2018-03-16 21:03:02 -0700

    update of spi driver

--- main.cpp
+++ main.cpp
@@ -1,175 +1,148 @@
 #include "FreeRTOS.h"
 #include "tasks.hpp"
-#include "uart0_min.h"
-#include "stdio.h"
 #include "LPC17xx.h"
-#include "lpc_isr.h"
+#include "stdio.h"
+#include "printf_lib.h"
 
-#include "io.hpp"
-#include "uartDriver.hpp"
+#include "gpioStruct.hpp"
+#include "gpioDriver.hpp"
+#include "spiDriver.hpp"
 
+#include "semphr.h"
 
-LabUART UART_PORT;
+//Semaphore
+SemaphoreHandle_t spi_bus_lock = xSemaphoreCreateMutex();
 
-QueueHandle_t charQueue;
+//Data Sizes
+const uint8_t four_bit      = 0x3;
+const uint8_t five_bit      = 0x4;
+const uint8_t six_bit       = 0x5;
+const uint8_t seven_bit     = 0x6;
+const uint8_t eight_bit     = 0x7;
+const uint8_t nine_bit      = 0x8;
+const uint8_t ten_bit       = 0x9;
+const uint8_t eleven_bit    = 0xA;
+const uint8_t twelve_bit    = 0xB;
+const uint8_t thirteen_bit  = 0xC;
+const uint8_t fourteen_bit  = 0xD;
+const uint8_t fifteen_bit   = 0xE;
+const uint8_t sixteen_bit   = 0xF;
 
-void print_char(void* pvparams)
+bool active_flag0 = false; 
+adlx_t status_reg; 
+
+void task_sig_reader(void *p)
 {
-  char i = 0;
+  const uint8_t expected1 = 0x1F;
+  const uint8_t expected2 = 0x26; 
   
-  while(1)
-    {	
-      UART_PORT.transmit('5');
-      UART_PORT.transmit('4');
-      UART_PORT.transmit('+');
-      
-      UART_PORT.transmit('7');
-      UART_PORT.transmit('6');
-      UART_PORT.transmit('-');
-      
-      UART_PORT.transmit('9');
-      UART_PORT.transmit('9');
-      UART_PORT.transmit('*');
-      vTaskDelay(2000);
-    }
-}
-
-void print_with_buttons(void * pvparams)
-  {
-    int period = 0; 
-    int count = 0;
-    char ALU[3] = {'+', '-', '*'};
-    int  alu_count = 0; 
-    while(1)
-      {
-	if(SW.getSwitch(1))
-	  {
-	    count++; 
-	  }
-	if(SW.getSwitch(2) && count > 0 )
-	  {
-	    count--; 
-	  }
-	if(SW.getSwitch(3))
-	  {
-	    alu_count++;
-	    alu_count %= 3;
-	  }
-	if(SW.getSwitch(4))
-	  {
-	    if(period != 2)
-	      {
-		UART_PORT.transmit(count + '0');
-		period++;
-	      }
-	    else
-	      {
-		UART_PORT.transmit(ALU[alu_count]);
-		period = 0;
-	      }
-	  }
-	LD.setNumber(count);
-	vTaskDelay(200);
-      }
-  }
-
-uint8_t make_result(char* buffer)
-{
-  const char operand_1 = buffer[0];
-  const char operand_2 = buffer[1];
-  const char operation = buffer[2];
-  const uint8_t operand_1_val = operand_1 - '0';
-  const uint8_t operand_2_val = operand_2 - '0';
-  uint8_t answer = 0;
+  /* Get Parameter */
+  GPIO_SPI_Package *package;
+  package = (GPIO_SPI_Package*) p;
   
-  switch(operation)
+  while (1)
     {
-    case '+':
-      answer = operand_1_val + operand_2_val;
-      break;
-    case '-':
-      answer = operand_1_val - operand_2_val;
-      break;
-    case '*':
-      answer = operand_1_val * operand_2_val;
-      break;
-    default:
-      break;
-    }
-  
-  return answer;
-}
-
-void read_char(void* pvparams)
-{
-  char c;
-  int i = 0;
-  char input[3] = { 0 };
-  char buf[2] = { 0 };
-  uint8_t answer;
-  
-  while(1)
-    {
-      if(xQueueReceive(charQueue, &c, 1000))
+      if(xSemaphoreTake(spi_bus_lock, 1))
 	{
-	  printf("Got %c\n", c);
-	  input[i] = c;
-	  ++i;
+	  // Use Guarded Resource
+	  // Note that we may get interrupted here and the other task may corrupt our transfer
+	  package->gpio_object->setLow();
+	  uint8_t s1 = package->spi_object->transfer(0x9F);
+	  uint8_t s2 = package->spi_object->transfer(0x00);
+	  uint8_t s3 = package->spi_object->transfer(0x00);
+	  package->gpio_object->setHigh();
 	  
-	  if(i == 3)
+	  if (s2 != expected1 || s3 != expected2)
 	    {
-	      answer = make_result(input);							
-	      UART_PORT.transmit((char) answer);			
-	      printf("Sending back: %c %c %c = %d\n", input[0], input[2], input[1], answer);
-	      LD.setNumber(answer); 
-	      i = 0;
+	      u0_dbg_printf("Ooops... race condition\n");
+	      u0_dbg_printf("Manufacturer ID: %x\n", s2);
+	      u0_dbg_printf("Device ID: %x\n", s3);
+	      vTaskSuspend(NULL); // Suspend this task
 	    }
-	}
+	  else
+	    {
+	      u0_dbg_printf("Manufacturer ID: %x\n", s2);
+	      u0_dbg_printf("Device ID: %x\n", s3);
+	    }
+	  // Give Semaphore back:
+	  xSemaphoreGive(spi_bus_lock);
+	}	
+      vTaskDelay(1);
     }
 }
 
-void print_answer(void* pvparams)
+void task_page_reader(void *p)
 {
-  static char c;	
-  while(1)
+  GPIO_SPI_Package *package;
+  package = (GPIO_SPI_Package*) p;
+  
+  uint8_t page_size = 5; 
+  uint8_t read_page[page_size]; 
+  while (1)
     {
-      
-      if(xQueueReceive(charQueue, &c, 1000))
+      if(xSemaphoreTake(spi_bus_lock, 1))
 	{
-	  printf("Sender received: %d\n", (uint8_t) c);
+	  // Use Guarded Resource
+	  package->gpio_object->setLow();
+	  //read_512_byte_page();
+	  uint8_t s1 = package->spi_object->transfer(0xD2);
+	  uint8_t s2 = package->spi_object->transfer(0x0);  // Address byte
+	  uint8_t s3 = package->spi_object->transfer(0x0);  // Address byte
+	  uint8_t s4 = package->spi_object->transfer(0x0);  // Address byte
+	  uint8_t s5 = package->spi_object->transfer(0x0);  // dummy byte
+	  uint8_t s6 = package->spi_object->transfer(0x0);  // dummy byte
+	  uint8_t s7 = package->spi_object->transfer(0x0);  // dummy byte
+	  uint8_t s8 = package->spi_object->transfer(0x0);  // dummy byte
+	  for(int i = 0; i < page_size; i++)
+	    {
+	      read_page[i] = package->spi_object->transfer(0x0);
+	    }
+	  package->gpio_object->setHigh();
+	  for(int i = 0; i < page_size; i++)
+	    {
+	      u0_dbg_printf("page read %i: %x\n",i, read_page[i]);
+	    }
+	  // Give Semaphore back:
+	  xSemaphoreGive(spi_bus_lock);
 	}
-      LD.setNumber(c); 
+      vTaskDelay(1);
     }
-}
-
-void rx_int_handler(void)
-{
-  char c;
-  c = UART_PORT.receive();
-  xQueueSend(charQueue, &c, 1000);
 }
 
 int main(int argc, char const *argv[])
 {
-  LD.init();
-  LD.clear();
+  const uint32_t STACK_SIZE = 1024;
+  const uint8_t port = 0;
+  const uint8_t pin = 6; 
+  
+  // Declare SSP1
+  LabSPI *mySSP1 = new LabSPI();
+  mySSP1->init(mySSP1->SSP1, eight_bit , mySSP1->SPI, 4);
+  u0_dbg_printf("SSPI: %i, FORMAT: %i\n", mySSP1->SSP1, mySSP1->SPI);
+  
+  // Declare GPIO
+  LabGPIO *myGPIO_0_6 = new LabGPIO(port, pin);
+  myGPIO_0_6->setAsOutput();
+  
+  // Declare Package
+  GPIO_SPI_Package * spi_package = new GPIO_SPI_Package; 
+  
+  /* Initialization Code */
+  //----------------internal GPIO, SSP1---------------------//
+  spi_package->globalVar = &active_flag0;
+  spi_package->spi_object = mySSP1;
+  spi_package->gpio_object = myGPIO_0_6;
 
-  SW.init(); 
+  // Print Status Register
+  status_reg = mySSP1->getStatusReg();
+  u0_dbg_printf("Status Register Read -->> TFE: %i, TNF: %i, RNE: %i, RFE %i, BSY: %i\n", status_reg.TFE, status_reg.TNF, status_reg.RNE, status_reg.RFE, status_reg.BSY);
 
-  UART_PORT.init(Peripheral::UART2);
   
-  isr_register(UART2_IRQn, rx_int_handler);
-  NVIC_EnableIRQ(UART2_IRQn);
+  xTaskCreate(task_sig_reader, "sig_reader", STACK_SIZE, (void *)spi_package, 1 | portPRIVILEGE_BIT, NULL );
+  xTaskCreate(task_page_reader, "page_reader", STACK_SIZE, (void *)spi_package, 1 | portPRIVILEGE_BIT, NULL );
 
-  charQueue = xQueueCreate(100, sizeof(char));
-  
-  scheduler_add_task(new terminalTask(PRIORITY_HIGH));
-  xTaskCreate(print_with_buttons, "print_with_buttons", 512, (void*) 1, 1, NULL );
-  xTaskCreate(print_answer, "answer", 512, (void*) 1, 1, NULL );
-  //  xTaskCreate(read_char, "read_char", 512, (void*) 1, 1, NULL);
-  
-  
-  scheduler_start();
-  
-  return false;
+  // Start Scheduler
+  vTaskStartScheduler();
+
+  return -1;
 }
