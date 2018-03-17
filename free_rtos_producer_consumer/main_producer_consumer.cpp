eccd2c51a534152d901757e3cfa4d0eaa73839ce
jcook3701 <jcook3701@gmail.com>
2018-03-17 00:17:44 -0700

    updates to free_rtos_producer_consumer

--- free_rtos_producer_consumer/main_producer_consumer.cpp
+++ free_rtos_producer_consumer/main_producer_consumer.cpp
@@ -1,7 +1,8 @@
 #include "FreeRTOS.h"
 #include "tasks.hpp"
-#include "LPC17xx.h"
 #include "stdio.h"
+
+#include "producer_consumer.hpp"
 
 QueueHandle_t q;
 
@@ -30,12 +31,15 @@ void consumer(void *p) /* HIGH priority */
 
 int main(int argc, char const *argv[])
 {
+  const uint32_t STACK_SIZE = 1024;
+
   // Queue handle is not valid until you create it
   q = xQueueCreate(10, sizeof(int));
 
+  producer_consumer_package *package = new producer_consumer_package; 
     
-  xTaskCreate(producer, "producer", STACK_SIZE, (void *)spi_package, 1 | portPRIVILEGE_BIT, NULL );
-  xTaskCreate(consumer, "consumer", STACK_SIZE, (void *)spi_package, 1 | portPRIVILEGE_BIT, NULL );
+  xTaskCreate(producer, "producer", STACK_SIZE, (void *)package, 1 | portPRIVILEGE_BIT, NULL );
+  xTaskCreate(consumer, "consumer", STACK_SIZE, (void *)package, 1 | portPRIVILEGE_BIT, NULL );
 
 
   return -1; 
